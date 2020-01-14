af76199f826da0d34affb5d39a0cda27c8bdf7b2
dancasas <dan.casas@gmail.com>
2020-01-14 15:30:49 +0100

    Update 1_2_hermite_interpolation_solution.m

--- solutions/1_2_hermite_interpolation_solution.m
+++ solutions/1_2_hermite_interpolation_solution.m
@@ -1,4 +1,4 @@
-function 1_2_hermite_interpolation_solution(h0, h1, h2, h3)
+function 1_2_hermite_interpolation_solution(f_0, f_1, f_0_deriv, f_1_deriv)
     x = 0:0.0001:1;
     figure;
     hold on;
@@ -11,7 +11,7 @@ function 1_2_hermite_interpolation_solution(h0, h1, h2, h3)
     H = [2 -3 0 1; -2 3 0 0; 1 -2 1 0; 1 -1 0 0]';
     
     % h
-    h = [h0; h1; h2; h3];
+    h = [f_0; f_1; f_0_deriv; f_1_deriv];
     
     sol = H*h;
     y = sol(1) * x.^3 + sol(2) * x.^2 + sol(3) * x + sol(4)*1 ;
