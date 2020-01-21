fc5e1d9f43cbee306d488f79fa58c74502133eb6
Dan Casas <dan.casas@gmail.com>
2020-01-21 15:52:41 +0100

    D

--- solutions/task_3_4_compute_global_blendshape_solution.m
+++ solutions/task_3_4_compute_global_blendshape_solution.m
@@ -5,9 +5,8 @@ function task_3_4_compute_global_blendshape_solution()
      
     % Reads and vectorizes mesh
     [V00,F] = read_vertices_and_faces_from_obj_file(strcat(blendshape_path,'/neutral-tri.obj'));
-    V00 = vertcat(V00(:,1),V00(:,2),V00(:,3));
-    
     num_vertices = size(V00,1);
+    V00 = vertcat(V00(:,1),V00(:,2),V00(:,3));
     
     % Reads and vectorizes mesh 02
     [V02,F] = read_vertices_and_faces_from_obj_file(strcat(blendshape_path, '/left-smile-tri.obj'));
@@ -88,4 +87,58 @@ function task_3_4_compute_global_blendshape_solution()
         
         drawnow;
     end
+end
+
+
+function [V,F] = read_vertices_and_faces_from_obj_file(filename)
+  % From: http://www.alecjacobson.com/weblog/?p=917
+  % Reads a .obj mesh file and outputs the vertex and face list
+  % assumes a 3D triangle mesh and ignores everything but:
+  % v x y z and f i j k lines
+  % Input:
+  %  filename  string of obj file's path
+  %
+  % Output:
+  %  V  number of vertices x 3 array of vertex positions
+  %  F  number of faces x 3 array of face indices
+  %
+  V = zeros(0,3);
+  F = zeros(0,3);
+  vertex_index = 1;
+  face_index = 1;
+  fid = fopen(filename,'rt');
+  line = fgets(fid);
+  while ischar(line)
+    vertex = sscanf(line,'v %f %f %f');
+    face = sscanf(line,'f %d %d %d');
+    face_long = sscanf(line,'f %d//%d %d//%d %d//%d',6);
+    face_long_long = sscanf(line,'f %d/%d/%d %d/%d/%d %d/%d/%d',9);
+
+    % see if line is vertex command if so add to vertices
+    if(size(vertex)>0)
+      V(vertex_index,:) = vertex;
+      vertex_index = vertex_index+1;
+   % see if line is simple face command if so add to faces
+    elseif(size(face,1)==3)
+      F(face_index,:) = face;
+      face_index = face_index+1;
+    % see if line is a face with normal indices command if so add to faces
+    elseif(size(face_long,1)==6)
+      % remove normal
+      face_long = face_long(1:2:end);
+      F(face_index,:) = face_long;
+      face_index = face_index+1;
+    % see if line is a face with normal and texture indices command if so add to faces
+    elseif(size(face_long_long,1)==9)
+      % remove normal and texture indices
+      face_long_long = face_long_long(1:3:end);
+      F(face_index,:) = face_long_long;
+      face_index = face_index+1;
+    else
+      fprintf('Ignored: %s',line);
+    end
+
+    line = fgets(fid);
+  end
+  fclose(fid);
 end
